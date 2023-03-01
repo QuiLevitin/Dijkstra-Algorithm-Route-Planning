93204a14604818abb9f87c6beee06f0968251174
Jerald E. Casulla <dlarejcasulla12@gmail.com>
2023-03-01 19:30:04 +0800

    DARP Final

---
+++ DARP/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void ActiveFront()
+    {
+        Path.foundLines.Clear();
+        SceneManager.LoadScene("FrontScene");
+    }
+
+    public void ActiveMain()
+    {
+        Path.foundLines.Clear();
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ActiveCreateMainWithBG()
+    {
+        Path.foundLines.Clear();
+        SceneManager.LoadScene("MainWithBG");
+    }
+
+    public void ActiveLoad()
+    {
+        Path.foundLines.Clear();
+        SceneManager.LoadScene("LoadScene");
+    }
+
+
+    public void ActiveDefault()
+    {
+        Path.foundLines.Clear();
+        SceneManager.LoadScene("default");
+    }
+    
+    public void ExitGame()
+    {
+        Path.foundLines.Clear();
+        Application.Quit();
+    }
+
+    public void ResetPrefs()
+    {
+        Path.foundLines.Clear();
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void ClearCacheApp()
+    {
+        Path.foundLines.Clear();
+        Caching.ClearCache();
+    }
+}
---
+++ DARP/Assets/Scripts/SettingsButtonManagement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SettingsButtonManagement : MonoBehaviour
+{
+
+    
+
+    [SerializeField] private Slider sizeSlider;
+    [SerializeField] private Slider edgeSlider;
+    [SerializeField] private Slider pathSlider;
+    
+ 
+
+
+    void Start()
+    {
+        sizeSlider.value = PlayerPrefs.GetFloat("sizeSettings");
+        edgeSlider.value = PlayerPrefs.GetFloat("edgeSettings");
+        pathSlider.value = PlayerPrefs.GetFloat("pathSettings");
+        
+
+
+    }
+
+
+    void Update()
+    {
+        
+
+        PlayerPrefs.SetFloat("sizeSettings", sizeSlider.value); 
+        PlayerPrefs.SetFloat("edgeSettings", edgeSlider.value); 
+        PlayerPrefs.SetFloat("pathSettings", pathSlider.value); 
+
+
+
+    }
+
+
+
+
+
+}
+
+ 
